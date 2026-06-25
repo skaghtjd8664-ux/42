07599b4936cd2aa2b4b594fc4ac2167c7eb4ad28
skaghtjd8664-ux <skaghtjd8664@gmail.com>
2026-06-25 12:37:18 +0900

    Update ft_printf.hex.c

--- ft_printf/ft_printf.hex.c
+++ ft_printf/ft_printf.hex.c
@@ -1,41 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.hex.c                                    :+:      :+:    :+:   */
+/*   ft_printf_char.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: honam <honam@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/25 11:30:29 by honam             #+#    #+#             */
-/*   Updated: 2026/06/25 12:05:13 by honam            ###   ########.fr       */
+/*   Created: 2026/06/23 14:49:53 by marvin            #+#    #+#             */
+/*   Updated: 2026/06/23 14:49:53 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf_hex(unsigned int n, char type)
+int	ft_printf_char(char c)
 {
-	int		len;
-	char	*base;
-	int		temp;
-	int		idx;
-
-	idx = 0;
-	len = 0;
-	if (type == 'X')
-		base = "0123456789ABCDEF";
-	else
-		base = "0123456789abcdef";
-	if (n > 15)
-	{
-		temp = ft_pritnf_hex(n / 16, type);
-		if (temp == -1)
-			return (-1);
-		len += temp;
-	}
-	if (write(1, &base[idx], 1) == -1)
-		return (-1);
-	len++;
-	return (len);
+	write(1, &c, 1);
 }
 
-int	ft_printf_ptr(void *p);
+int	ft_printf_str(const char *str)
+{
+	int	len;
+
+	if (str[len] == '\0')
+	{
+		if (write(1, "(null)", 6) == -1)
+			return (-1);
+		return (6);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		if (write(1, &str[len], 1) == -1)
+			return (-1);
+		len++;
+	}
+	return (len);
+}
